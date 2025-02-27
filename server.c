1657b81235fc9d3055e048a3df8c64050ccdab7d
Oliver Krisetya <okrisetya@gmail.com>
2025-02-26 20:52:56 -0500

    Client credentials

--- server.c
+++ server.c
@@ -3,26 +3,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
 
-// int fdtable[16];
+#define SOCKET_PATH "/tmp/socket"
+struct ucred
+{
+    pid_t pid;
+    uid_t uid;
+    gid_t gid;
+};
 
 void handle_client(int client_fd)
 {
     printf("Client %d connected\n", client_fd);
+
+    struct ucred creds;
+    socklen_t len = sizeof(creds);
+    if (getsockopt(client_fd, SOL_SOCKET, LOCAL_PEERCRED, &creds, &len) < 0)
+    {
+        perror("getsockopt failed");
+    }
+    else
+    {
+        printf("Client UID: %d, GID: %d, PID: %d\n", creds.uid, creds.gid, creds.pid);
+    }
 }
 
 int main() {
+
     // Create the socket
     int socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    unlink(SOCKET_PATH);
 
     struct sockaddr_un *addr = malloc(sizeof(struct sockaddr_un));
     memset(addr, 0, sizeof(struct sockaddr_un));
 
-
     addr->sun_family = AF_UNIX;
-    strncpy(addr->sun_path, "/tmp/socket", sizeof(addr->sun_path) - 1);
-    
+    strncpy(addr->sun_path, SOCKET_PATH, sizeof(addr->sun_path) - 1);
 
+    unlink(SOCKET_PATH);
     // Bind the socket
     if (bind(socket_fd, (struct sockaddr *)addr, sizeof(struct sockaddr_un)) < 0) {
         perror("Bind failed");
