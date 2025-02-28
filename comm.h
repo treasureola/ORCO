66e1b5cf584314fe323bca8a5386455f322097d9
Cynthia Driskell <76992666+scoledriskell@users.noreply.github.com>
2025-02-28 23:48:50 +0000

    Defined _GNU_SOURCE in comm.h to target GNU/Linux

--- comm.h
+++ comm.h
@@ -1,18 +1,16 @@
-
+#ifndef H_ORCO_COMMON
+#define H_ORCO_COMMON
+#define _GNU_SOURCE
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <bits/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 
 #define SOCKET_PATH "/tmp/socket"
-
-struct ucred
-{
-    pid_t pid;
-    uid_t uid;
-    gid_t gid;
-};
+#endif
+;
