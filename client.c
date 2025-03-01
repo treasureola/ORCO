3fd364adf17ed3d99f7a644bd4875ad717b8d57f
Olamide T. Oluwalalde <treasurelade1@gmail.com>
2025-03-01 16:53:28 -0500

    updated client

--- client.c
+++ client.c
@@ -1,36 +1,85 @@
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "comm.h"
 
 int main(){
+    pid_t pid = getpid();
+    uid_t uid = getuid();
+    gid_t gid = getgid();
+    
+    printf("Client pid: %d\n", pid);
+    printf("Client uid: %d\n", uid);
+    printf("Client gid: %d\n", gid);
+
     // Create the socket
     int socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (socket_fd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-    struct sockaddr_un *addr = malloc(sizeof(struct sockaddr_un));
-    memset(addr, 0, sizeof(struct sockaddr_un));
-    addr->sun_family = AF_UNIX;
-    strncpy(addr->sun_path, "/tmp/socket", sizeof(addr->sun_path) - 1);
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(struct sockaddr_un));
+    addr.sun_family = AF_UNIX; // Change addr-> to addr.
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
 
-    printf("Client connecting\n");
     // Connect to the server
-    int status = connect(socket_fd, (struct sockaddr *)addr, sizeof(struct sockaddr_un));
+    int status = connect(socket_fd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un));
     if (status < 0) {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
     
-    printf("Connected to server\n");
+    // Send credentials to the server
+    struct ucred creds;
+    creds.pid = pid;
+    creds.uid = uid;
+    creds.gid = gid;
 
-    // // Send a message to the server
-    // char *message = "Hello, server!";
-    // send(socket_fd, message, strlen(message), 0);
+    struct msghdr msgh;
+    memset(&msgh, 0, sizeof(struct msghdr));
 
-    // // Receive a message from the server
-    // char buffer[1024];
-    // recv(socket_fd, buffer, sizeof(buffer), 0);
-    // printf("Received: %s\n", buffer);
+    int enable = 1;
+    setsockopt(socket_fd, SOL_SOCKET, SO_PASSCRED, &enable, sizeof(enable));
+
+    struct iovec iov;
+    char dummy = 0; // Dummy byte to satisfy `msg_iov`
+
+    iov.iov_base = &dummy;
+    iov.iov_len = sizeof(dummy);
+    msgh.msg_iov = &iov;
+    msgh.msg_iovlen = 1;
+
+    // Allocate control message buffer
+    size_t controlMsgSize = CMSG_SPACE(sizeof(struct ucred));
+    char *controlMsg = malloc(controlMsgSize);
+    if (!controlMsg)
+    {
+        perror("Memory allocation failed");
+        close(socket_fd);
+        exit(EXIT_FAILURE);
+    }
+    memset(controlMsg, 0, controlMsgSize);
+
+    msgh.msg_control = controlMsg;
+    msgh.msg_controllen = controlMsgSize;
+    
+    struct cmsghdr *cmsgp = CMSG_FIRSTHDR(&msgh);
+    cmsgp->cmsg_level = SOL_SOCKET;
+    cmsgp->cmsg_type = SCM_CREDENTIALS;
+    cmsgp->cmsg_len = CMSG_LEN(sizeof(struct ucred));
+
+    memcpy(CMSG_DATA(cmsgp), &creds, sizeof(struct ucred));
+
+    ssize_t bytesSent = sendmsg(socket_fd, &msgh, 0);
+    printf("Client: Sent %ld bytes\n", bytesSent);
+    if (bytesSent < 0) {
+        perror("Failed to send credentials");
+        exit(EXIT_FAILURE);
+    }
+
+    // Close the socket
+    close(socket_fd);
+
+    free(controlMsg);
 
     return 0;
 }
